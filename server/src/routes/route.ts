cb284294a28a2aee73600f6a35b4026968026856
Yuvansh Chauhan <chauan.boby0211@gmail.com>
2025-11-09 20:38:40 +0530

    created lib folder

--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import {Router} from 'express';
 import * as controller from '../controller/controller.js';
 import { authenticateToken } from '../middleware/authmiddleware.js'; 
 
